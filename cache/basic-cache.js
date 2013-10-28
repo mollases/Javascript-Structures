fe9d7135a72730703e8cfd11a34785c2dccf89fb
Admas <molla.admas@gmail.com>
2013-10-27 19:05:34 -0500

    Added comments

--- cache/basic-cache.js
+++ cache/basic-cache.js
@@ -19,63 +19,64 @@ BasicCache.prototype.add = function(obj){
     this._decrementAges(indexOf);
   };
 
-  // adds an object to the cache. No rules are respected.
-  BasicCache.prototype._add = function(obj){
-    clog('force addition called');
-    this.list.push({'obj' : obj, age : 1});
-  };
+// adds an object to the cache. No rules are respected.
+BasicCache.prototype._add = function(obj){
+  clog('force addition called');
+  this.list.push({'obj' : obj, age : 1});
+};
 
-  // resets a given index to a new obj.
-  BasicCache.prototype._reset = function(index,obj){
-    clog('reseting index '+index);
-    this.list[index].obj = obj;
-    this.list[index].age = 1;
-  };
+// resets a given index to a new obj.
+BasicCache.prototype._reset = function(index,obj){
+  clog('reseting index '+index);
+  this.list[index].obj = obj;
+  this.list[index].age = 1;
+};
 
-  // searches the cache to find the obj and returns its index
-  // returns -1 if it didnt find anything.
-  BasicCache.prototype.contains = function(obj) {
-    clog('contains running on obj');
-    for (var i = 0; i < this.list.length; i++) {
-      if (this.list[i].obj === obj) {
-        clog('contains hit on index '+i);
-        return i;
-      }
+// searches the cache to find the obj and returns its index
+// returns -1 if it didnt find anything.
+BasicCache.prototype.contains = function(obj) {
+  clog('contains running on obj');
+  for (var i = 0; i < this.list.length; i++) {
+    if (this.list[i].obj === obj) {
+      clog('contains hit on index '+i);
+      return i;
     }
-    return -1;
-  };
-
-  // finds the newest entry and returns that index location
-  BasicCache.prototype._findNewestEntry = function(){
-    var minAge;
-    var minAgeIndex = -1;
-    for (var i = 0; i < this.list.length; i++) {
-      if(minAge === undefined || minAge >= this.list[i].age){
-        clog('minAge set to '+this.list[i].age);
-        minAge = this.list[i].age;
-        minAgeIndex = i;
-      }
-    }
-    return minAgeIndex;
-  };
-
-  // as caches are time sensitive by nature
-  // we decrement the age factor to get the least recently used on new additions.
-  BasicCache.prototype._decrementAges = function(lastChange){
-    clog('decrementing all ages')
-    for( var i = 0; i < this.list.length; i++){
-      this.list[i].age--;
-    }
-    this.list[lastChange].age++;
-  };
-
-  BasicCache.prototype.printInfo = function(){
-    clog('cap = '+this.cap);
-    for(var i = 0; i < this.list.length; i++){
-      clog('{obj: '+this.list[i].obj+', age: '+this.list[i].age+'}');
-    }
-    clog('---');
   }
+  return -1;
+};
+
+// finds the newest entry and returns that index location
+BasicCache.prototype._findNewestEntry = function(){
+  var minAge;
+  var minAgeIndex = -1;
+  for (var i = 0; i < this.list.length; i++) {
+    if(minAge === undefined || minAge >= this.list[i].age){
+      clog('minAge set to '+this.list[i].age);
+      minAge = this.list[i].age;
+      minAgeIndex = i;
+    }
+  }
+  return minAgeIndex;
+};
+
+// as caches are time sensitive by nature
+// we decrement the age factor to get the least recently used on new additions.
+BasicCache.prototype._decrementAges = function(lastChange){
+  clog('decrementing all ages')
+  for( var i = 0; i < this.list.length; i++){
+    this.list[i].age--;
+  }
+  this.list[lastChange].age++;
+};
+
+// prints out useful information regarding the BasicCache and its contents.
+BasicCache.prototype.printInfo = function(){
+  clog('cap = '+this.cap);
+  for(var i = 0; i < this.list.length; i++){
+    clog('{obj: '+this.list[i].obj+', age: '+this.list[i].age+'}');
+  }
+  clog('---');
+}
 
 // console.log alias
 function clog(msg){
